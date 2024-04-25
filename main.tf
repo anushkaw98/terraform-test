eef9aa9796ea163d47a1092eb064e0b31ca23d46
anushkaw98 <137258770+anushkaw98@users.noreply.github.com>
2024-04-25 16:42:33 +0530

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,137 @@
+provider "aws" {
+  region  = "us-east-1"
+  access_key = "AKIAS6M2R62IJRTLOZMC"
+  secret_key = "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"
+}
+
+# 1.create vpc
+resource "aws_vpc" "prod-vpc" {
+  cidr_block = "10.0.0.0/16"
+  
+}
+
+#2. create internet gateway
+resource "aws_internet_gateway" "gw" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+}
+
+#create custom route table
+resource "aws_route_table" "prod-route-table" {
+  vpc_id = aws_vpc.prod-vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.gw.id
+  }
+
+  route {
+    ipv6_cidr_block        = "::/0"
+    gateway_id             = aws_internet_gateway.gw.id
+  }
+
+  tags = {
+    Name = "prod"
+  }
+}
+
+variable "set_subnet" {
+  description = "cidr block for the subnription"
+}
+
+# 4.create subnet 
+
+resource "aws_subnet" "subnet-1" {
+    vpc_id = aws_vpc.prod-vpc.id
+    cidr_block = var.set_subnet
+    availability_zone = "us-east-1a"
+
+    tags = {
+        Name = "prod-subnet"
+    }
+}
+
+#5.associate subnet with route table
+resource "aws_route_table_association" "a" {
+    subnet_id = aws_subnet.subnet-1.id
+    route_table_id = aws_route_table.prod-route-table.id
+}
+
+#6.create a security group to allow port 22,443, etc.
+resource "aws_security_group" "allow_web" {
+  name        = "allow_web_traffic"
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = aws_vpc.prod-vpc.id
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_https" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 443
+  ip_protocol       = "tcp"
+  to_port           = 443
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_http" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 80
+  ip_protocol       = "tcp"
+  to_port           = 80
+}
+
+resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  from_port         = 22
+  ip_protocol       = "tcp"
+  to_port           = 22
+}
+resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
+  security_group_id = aws_security_group.allow_web.id
+  cidr_ipv4         = "0.0.0.0/0"
+  ip_protocol       = "-1" # semantically equivalent to all ports
+}
+
+#7.create a network interface with an Ip in the subnet that was created in step 4
+resource "aws_network_interface" "web-server-nic" {
+  subnet_id       = aws_subnet.subnet-1.id
+  private_ips     = ["10.0.1.50"]
+  security_groups = [aws_security_group.allow_web.id]
+
+}
+
+#8.assign an elastic IP to the network interface created in step 7
+resource "aws_eip" "two" {
+    domain                    = "vpc"
+    network_interface         = aws_network_interface.web-server-nic.id
+    associate_with_private_ip = "10.0.1.50"
+    depends_on = [aws_internet_gateway.gw]
+}
+
+#9.create ubuntu server and install/enable apache2
+resource "aws_instance" "web-server-instance" {
+    ami = "ami-080e1f13689e07408"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    key_name = "aws-accesskey-ever"
+
+    network_interface {
+      device_index = 0
+      network_interface_id = aws_network_interface.web-server-nic.id
+
+    }
+
+    user_data = <<-EOF
+                #!/bin/bash
+                sudo apt update -y
+                sudo apt install apache2 -y
+                sudo systemctl start apache2
+                sudo systemctl enable apache2
+                sudo bash -c 'echo first web server > /var/www/html/index.html'
+                EOF
+
+    tags = {
+        Name = "web-server"
+    }
+ }
