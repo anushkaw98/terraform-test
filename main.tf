062b089603de2722615741d159e1b0771aa8a73c
anushkaw98 <137258770+anushkaw98@users.noreply.github.com>
2024-04-26 10:20:56 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -57,41 +57,45 @@ resource "aws_route_table_association" "a" {
     route_table_id = aws_route_table.prod-route-table.id
 }
 
-#6.create a security group to allow port 22,443, etc.
-resource "aws_security_group" "allow_web" {
-  name        = "allow_web_traffic"
-  description = "Allow TLS inbound traffic and all outbound traffic"
-  vpc_id      = aws_vpc.prod-vpc.id
-}
+ # 6. Create Security Group to allow port 22,80,443
+ resource "aws_security_group" "allow_web" {
+   name        = "allow_web_traffic"
+   description = "Allow Web inbound traffic"
+   vpc_id      = aws_vpc.prod-vpc.id
 
-resource "aws_vpc_security_group_ingress_rule" "allow_https" {
-  security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 443
-  ip_protocol       = "tcp"
-  to_port           = 443
-}
+   ingress {
+     description = "HTTPS"
+     from_port   = 443
+     to_port     = 443
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
+     description = "HTTP"
+     from_port   = 80
+     to_port     = 80
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
+   ingress {
+     description = "SSH"
+     from_port   = 22
+     to_port     = 22
+     protocol    = "tcp"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_http" {
-  security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 80
-  ip_protocol       = "tcp"
-  to_port           = 80
-}
+   egress {
+     from_port   = 0
+     to_port     = 0
+     protocol    = "-1"
+     cidr_blocks = ["0.0.0.0/0"]
+   }
 
-resource "aws_vpc_security_group_ingress_rule" "allow_ssh" {
-  security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.allow_web.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
-}
+   tags = {
+     Name = "allow_web"
+   }
+ }
 
 #7.create a network interface with an Ip in the subnet that was created in step 4
 resource "aws_network_interface" "web-server-nic" {
