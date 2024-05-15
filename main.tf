48efe8e0e7664ff3c82b8789669bcd1da87edec9
anushkaw98 <anushkaw98@gmail.com>
2024-05-15 10:20:05 +0530

    test

--- main.tf
+++ main.tf
@@ -2,3 +2,11 @@ provider "aws" {
   region  = "us-east-1"
 }
 
+resource "aws_subnet" "private" {
+  vpc_id     = vpc-0bbc049933485ce0a
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "private"
+  }
+}
